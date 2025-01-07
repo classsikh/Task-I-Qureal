5247f941a6fbffcc1b77f88dd1a7caac01829fdc
GAGANPREET SINGH CHHABRA <108130370+classsikh@users.noreply.github.com>
2025-01-08 03:15:09 +0530

    Update usePokemons.js

--- src/queries/usePokemons.js
+++ src/queries/usePokemons.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchPokemons = async () => {
-  const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1000");
+  const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100");
   return response.data.results;
 };
 
